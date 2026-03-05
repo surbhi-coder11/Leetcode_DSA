1b714a24016f844cffa28c85b45927e1001db0c9
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-05 23:17:22 +0530

    Time: 19 ms (93.46%), Space: 83.3 MB (50.11%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        // int n = nums.length;
+
+        // for(int i=0;i<n-1;i++){
+
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[j]<nums[i]){
+        //             int temp = nums[i];
+        //             nums[i]= nums[j];
+        //             nums[j] = temp;
+        //         }
+        //     }
+        // }
+        return nums;
+    }
+}
