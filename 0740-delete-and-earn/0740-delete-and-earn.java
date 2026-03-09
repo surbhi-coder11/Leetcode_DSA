1ac9de9051a9610f0027a637d1861e97730ca338
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-09 14:21:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int deleteAndEarn(int[] nums) {
-        int max = 0;
-        for (int num : nums) {
-            max = Math.max(max, num);
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0)
+                dp[i][0] = dp[i - 1][0];
         }
 
-        int[] sum = new int[max + 1];
-        for (int num : nums) {
-            sum[num] += num;
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0)
+                dp[0][j] = dp[0][j - 1];
         }
 
-        int take = 0, skip = 0;
-
-        for (int i = 0; i <= max; i++) {
-            int takei = skip + sum[i];
-            int skipi = Math.max(skip, take);
-            take = takei;
-            skip = skipi;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
         }
 
-        return Math.max(take, skip);
+        return dp[m - 1][n - 1];
     }
 }
