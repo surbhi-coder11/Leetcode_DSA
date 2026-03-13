c8b9d6494091d401d7c502b6548110c994836d0b
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-13 22:44:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -18,24 +18,40 @@ class Solution {
 
     // optimized 
 
-    Arrays.sort(nums);
-    int  ans = nums[0];
-    int freq = 1;
-    for(int i=1;i<n;i++){
-        if(nums[i] == nums[i-1]){
+    // Arrays.sort(nums);
+    // int  ans = nums[0];
+    // int freq = 1;
+    // for(int i=1;i<n;i++){
+    //     if(nums[i] == nums[i-1]){
+    //         freq++;
+    //     }
+    //     else{
+    //         freq=1;
+    //         ans=nums[i];
+    //     }
+
+    //     if(freq>(n/2)){
+    //         return ans;
+    //     }
+    // }
+
+    //    return ans;
+
+     int ans = 0;
+     int freq =0;
+     for(int i=0;i<n;i++){
+        if(freq==0){
+            ans = nums[i];
+        }
+        if(nums[i]==ans){
             freq++;
         }
         else{
-            freq=1;
-            ans=nums[i];
+            freq--;
         }
+     }
 
-        if(freq>(n/2)){
-            return ans;
-        }
-    }
-
-       return ans;
+     return ans;
     
 
     }
