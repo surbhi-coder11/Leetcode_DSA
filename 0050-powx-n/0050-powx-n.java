7202f3da5c45679780236dba338bed20d39e0408
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-15 00:40:50 +0530

    Time: 1 ms (10.16%), Space: 48.2 MB (45.26%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        if(x==1) return 1.0;
+        if(x==-1 && n%2==0) return 1.0;
+        if(x==-1 && n%2!=0) return -1.0;
+        
+        double ans = 1;
+        long binform = n;
+        if(n<0){
+            x = 1/x;
+            binform = -binform;
+        }
+        while(binform>0){
+            if(binform%2==1){
+                ans = ans*x;
+            }
+            x*=x;
+            binform/=2;
+        }
+        return ans;
+    }
+}
