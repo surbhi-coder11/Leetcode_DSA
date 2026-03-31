81b8e9dab3656c7593639dbed1a9d6b663322553
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-31 14:42:53 +0530

    Time: 7 ms (22.08%), Space: 45.7 MB (34.23%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,10 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i=1;i<nums.length;i++){
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i+=3){
             if(nums[i]!=nums[i-1]){
                 return nums[i-1];
             }
         }
-        return nums[nums.length-1]
+        return nums[nums.length-1];
     }
 }
