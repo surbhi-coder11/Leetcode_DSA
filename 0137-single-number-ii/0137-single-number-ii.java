bae0c4f979df2e3f84d95f4ee6fae4890cfb68ea
Surbhi Gupta <bablooasr73@gmail.com>
2026-03-31 14:40:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1]
+    }
+}
