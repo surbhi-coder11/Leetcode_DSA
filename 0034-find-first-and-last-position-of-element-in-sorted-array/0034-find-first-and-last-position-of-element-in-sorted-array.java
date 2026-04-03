6549214af7808d56e49e1d2871fdf0bd47b8e560
Surbhi Gupta <bablooasr73@gmail.com>
2026-04-03 19:31:47 +0530

    Time: 1 ms (18.28%), Space: 48.9 MB (7.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int st =0;
-        int en = nums.length-1;
-        int idx=0;
-
-        while(st<en){
-           int mid = (st+en)/2;
-           if(nums[mid]>target){
-            en = mid-1;
-           }
-           else if(nums[mid]<target){
-            st = mid+1;
-           }
-           else{
-               idx= mid;
-               
-           }
-
+         int[] ans = new int[2];
+         Arrays.fill(ans,-1);
+         if(nums.length!=0){
+         ans[0] = firstocc(nums,target,0);
+         ans[1] =lastocc(nums,target,nums.length-1);
+         }
+         return ans;
+    }
+    private static int lastocc(int[] arr, int tar, int idx){
+        if(idx==0 && arr[idx]!=tar){
+            return -1;
         }
+        if(arr[idx]==tar){
+            return idx;
+        }
+        return lastocc(arr,tar,idx-1);
+    }
+    private static int firstocc(int[] arr, int tar, int idx){
+        if(idx==arr.length){
+            return -1;
+        }
+        if(arr[idx]==tar){
+            return idx;
+        }
+
+        return firstocc(arr,tar,idx+1);
     }
 }
