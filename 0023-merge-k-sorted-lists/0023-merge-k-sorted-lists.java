d932be1e4ffb30182b0ed404e5ed51d460972949
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-16 00:55:29 +0530

    Time: 1 ms (99.96%), Space: 46.9 MB (29.22%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return divideAndConquer(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divideAndConquer(ListNode[] lists, int start, int end) {
+        if (start == end) return lists[start];
+        if (start > end) return null;
+
+        int mid = start + (end - start) / 2;
+        ListNode left = divideAndConquer(lists, start, mid);
+        ListNode right = divideAndConquer(lists, mid + 1, end);
+
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
