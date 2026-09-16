ed6cbe900424d74dd5e8bad5c6fc0e2c27e3e3ee
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-16 18:40:51 +0530

    Time: 1 ms (99.96%), Space: 46.9 MB (46.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,36 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-        return divideAndConquer(lists, 0, lists.length - 1);
-    }
-
-    private ListNode divideAndConquer(ListNode[] lists, int start, int end) {
-        if (start == end) return lists[start];
-        if (start > end) return null;
-
-        int mid = start + (end - start) / 2;
-        ListNode left = divideAndConquer(lists, start, mid);
-        ListNode right = divideAndConquer(lists, mid + 1, end);
-
-        return mergeTwoLists(left, right);
-    }
-
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
-            }
-            curr = curr.next;
+        if(lists == null || lists.length ==0){
+            return null;
         }
+        return mergeinrange(lists,0,lists.length-1);
+    }
+    private ListNode mergeinrange(ListNode[] lists, int st ,  int en){
+        if(st==en){
+            return lists[st];
+        }
+        int mid = st + (en-st)/2;
+        ListNode leftlist = mergeinrange(lists,st, mid);
+        ListNode rightlist = mergeinrange(lists,mid+1,en);
+        
+        return mergenodes(leftlist,rightlist);
+        
+    }
+    private ListNode mergenodes(ListNode head1 , ListNode head2){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr =  dummy;
 
-        curr.next = (l1 != null) ? l1 : l2;
-        return dummy.next;
+        while(head1!=null && head2!=null){
+            if(head1.val>=head2.val){
+                curr.next = head2;
+                curr = curr.next;
+                head2 = head2.next;
+            }else{
+                curr.next = head1;
+                curr = curr.next;
+                head1 = head1.next;
+            }
+        }
+      while(head1!=null){
+         curr.next = head1;
+         curr = curr.next;
+         head1 = head1.next;
+      }
+      while(head2!=null){
+         curr.next = head2;
+          curr = curr.next;
+          head2 = head2.next;
+      }
+
+      return dummy.next;
+        
     }
 }
