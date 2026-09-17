5f534cd3417d010eee2fcdc0b2b79d66fd1651c6
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-17 11:28:02 +0530

    Time: 4 ms (36.37%), Space: 43.2 MB (72.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,31 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-
 class Solution {
     public boolean isValid(String s) {
-        // Odd length strings kabhi valid nahi ho sakti
-        if (s.length() % 2 != 0) {
+
+        if(s.length()%2 !=0){
             return false;
         }
+        HashMap<Character,Character> hp = new HashMap<>();
+        hp.put('}','{');
+        hp.put(')','(');
+        hp.put(']','[');
 
-        // Key: Closer -> Value: Matching Opener
-        HashMap<Character, Character> rule = new HashMap<>();
-        rule.put(')', '(');
-        rule.put(']', '[');
-        rule.put('}', '{');
+        Deque<Character> dq = new ArrayDeque<>();
 
-        Deque<Character> st = new ArrayDeque<>();
-
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-
-            // Agar ch closer nahi hai (matlab opener hai), toh push karo
-            if (!rule.containsKey(ch)) {
-                st.push(ch);
-            } else {
-                // Closer aane par: empty stack check aur type mismatch check
-                if (st.isEmpty() || st.pop() != rule.get(ch)) {
+            if(!hp.containsKey(ch)){
+                dq.push(ch);
+            }
+            else{
+                if(dq.isEmpty() || hp.get(ch)!=dq.pop()){
                     return false;
                 }
             }
-        }
 
-        // Aakhiri mein stack khali hona chahiye
-        return st.isEmpty();
+        }
+        if(!dq.isEmpty()){
+            return false;
+        }
+        return true;
     }
 }
