5eb8bc94b9a59d6ab6bc5794ad4f910ba01d2997
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-18 11:13:02 +0530

    Time: 1 ms (83.39%), Space: 43 MB (42.66%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,35 +1,34 @@
-import java.util.*;
-
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> countStack = new Stack<>();
-        Stack<StringBuilder> stringStack = new Stack<>();
-        StringBuilder current = new StringBuilder();
-        int k = 0;
+        Stack<Integer> count = new Stack<>();
+        Stack<StringBuilder> curr = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                k = k * 10 + (ch - '0');
-            } 
-            else if (ch == '[') {
-                countStack.push(k);
-                stringStack.push(current);
-                current = new StringBuilder();
-                k = 0;
-            } 
-            else if (ch == ']') {
-                int repeat = countStack.pop();
-                StringBuilder prev = stringStack.pop();
-                for (int i = 0; i < repeat; i++) {
-                    prev.append(current);
+        StringBuilder sb = new StringBuilder();
+        int k=0;
+
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                k = k*10 + (ch - '0');
+            }else if(ch == '['){
+                curr.push(sb);
+                count.push(k);
+                sb = new StringBuilder();
+                k=0;
+            }else if(ch == ']'){
+                StringBuilder decoded = curr.pop();
+                int rem = count.pop();
+
+                while(rem-->0){
+                    decoded.append(sb);
                 }
-                current = prev;
-            } 
-            else {
-                current.append(ch);
+
+                sb = decoded;
+            }
+            else{
+                sb.append(ch);
             }
         }
 
-        return current.toString();
+        return sb.toString();
     }
 }
