65187c86871fab1eb00e82fb1eb1f50b269941e1
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-18 23:54:59 +0530

    Time: 4 ms (73.38%), Space: 45.1 MB (70.38%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        // 1. Build next greater map using Monotonic Decreasing Stack
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Remaining elements in stack have no next greater
+        while (!stack.isEmpty()) {
+            nextGreaterMap.put(stack.pop(), -1);
+        }
+
+        // 2. Resolve queries for nums1 in O(1) each
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
