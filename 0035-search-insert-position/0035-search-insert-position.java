8d4eea6f5e6e4b7712030dc490c9e3d40a6f63e8
Surbhi Gupta <bablooasr73@gmail.com>
2026-09-22 12:11:12 +0530

    Time: 0 ms (100%), Space: 44.7 MB (66.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int lo =0;
-        int hi = nums.length -1;
-        while(lo<=hi){
-            int mid = lo+(hi - lo)/2;
-            if(nums[mid]>=target){
-                hi = mid-1;
-            }else{
-                lo=mid+1;
-            }
-        }
-        return lo;
+        int lo = 0, hi = nums.length - 1;
+      int ans = nums.length; // Default to n (agar saare elements target se chhote hon)
+
+while (lo <= hi) {
+    int mid = lo + (hi - lo) / 2;
+    if (nums[mid] >= target) {
+        ans = mid;        // mid feasible candidate hai, par kya isse pehle koi aur hai?
+        hi = mid - 1;     // Left side dhoondho
+    } else {
+        lo = mid + 1;     // nums[mid] chhota hai, right side jao
+    }
+}
+return ans; // ya direct return lo;
     }
 }
